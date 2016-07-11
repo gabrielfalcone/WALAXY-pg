bac61d3760aacc776837d0cdb7013a11624ea05c
Gabriel Falcone <gabrielfalcone@gmail.com>
2016-07-10 21:12:33 -0300

    change on platform browser

--- platforms/android/platform_www/cordova_plugins.js
+++ platforms/android/platform_www/cordova_plugins.js
@@ -7,13 +7,6 @@ module.exports = [
             "plugin.notification.badge",
             "cordova.plugins.notification.badge"
         ]
-    },
-    {
-        "file": "plugins/phonegap-plugin-push/www/push.js",
-        "id": "phonegap-plugin-push.PushNotification",
-        "clobbers": [
-            "PushNotification"
-        ]
     }
 ];
 module.exports.metadata = 
@@ -21,8 +14,7 @@ module.exports.metadata =
 {
     "cordova-plugin-whitelist": "1.2.2",
     "cordova-plugin-badge": "0.7.2",
-    "cordova-plugin-app-event": "1.2.0",
-    "phonegap-plugin-push": "1.7.4"
+    "cordova-plugin-app-event": "1.2.0"
 };
 // BOTTOM OF METADATA
 });
